45cf95d0963ab41ba213cb3dabf3140e0ea5302c
Jake Ross <jirhiker@gmail.com>
2021-11-30 17:07:07 -0700

    added nmbgmr package

--- nmbgmr/main.py
+++ nmbgmr/main.py
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ===============================================================================
-from .nmbgmr import NMBGMRLocations, NMBGMRThings
+from nmbgmr import NMBGMRLocations, NMBGMRThings
 
 
 def etl_locations(request):
